bd4463f0fea8e691409aa4b6afbe9bab5ca33ebd
Partha Pratim Saha <sahapratim2@gmail.com>
2023-08-31 11:15:18 -0500

    My First Project

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace API.Entities;
+
+public class AppUser
+{
+   
+    public int Id { get; set; }
+    public string UserName { get; set; }
+
+}
